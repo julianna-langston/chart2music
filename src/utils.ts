1a0ce70c5a20fc3f4ed2f5a5d6aca3c138ea68d1
Julianna Langston <julianna@cosaic.io>
2022-06-19 17:39:56 -0400

    Fix infinity in first example

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,7 @@ export const generateSummary = (title: string, x: AxisData, y: AxisData) =>
 
 export const calculateAxisMinimum = (data: dataPoint[][], prop: "x" | "y") => {
     const values: number[] = data.flat().map((point: dataPoint): number => {
-        if (point[prop] instanceof Number) {
+        if (typeof point[prop] === "number") {
             return point[prop] as number;
         }
         return Math.min(...Object.values(point[prop] as StatBundle));
@@ -33,7 +33,7 @@ export const calculateAxisMinimum = (data: dataPoint[][], prop: "x" | "y") => {
 };
 export const calculateAxisMaximum = (data: dataPoint[][], prop: "x" | "y") => {
     const values: number[] = data.flat().map((point: dataPoint): number => {
-        if (point[prop] instanceof Number) {
+        if (typeof point[prop] === "number") {
             return point[prop] as number;
         }
         return Math.max(...Object.values(point[prop] as StatBundle));
