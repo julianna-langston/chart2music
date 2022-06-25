23a018931144933f8950cdfed48e3705a8939968
Julianna Langston <julianna@cosaic.io>
2022-06-24 21:02:34 -0400

    Perfect test coverage of utils

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,5 @@
-import {calcPan, defaultFormat, generateSummary, interpolateBin, sentenceCase} from "../src/utils";
+import { dataPoint } from "../src/types";
+import {calcPan, calculateAxisMaximum, calculateAxisMinimum, defaultFormat, generateSummary, interpolateBin, sentenceCase} from "../src/utils";
 
 test("sentence case", () => {
     expect(sentenceCase("test")).toBe("Test");
@@ -38,3 +39,27 @@ test("adjust percent for panning", () => {
     expect(calcPan(0.5)).toBe(0);
     expect(calcPan(1)).toBe(0.98);
 });
+
+test("calculate axis min/max", () => {
+    const singleRow: dataPoint[][] = [[1, 5, 2, 0, 3, 6].map((x) => {return {x}})];
+    const multiRow = [
+        [1, 5, 2, 0, 3, 6].map((x) => {return {x}}),
+        [11, 15, 12, 10, 13, 16].map((x) => {return {x}}),
+    ];
+    const bundledRow = [[
+        {x: 5, y: {high: 50, low: 20}},
+        {x: 5, y: {high: 51, low: 15}},
+        {x: 5, y: {high: 52, low: 30}},
+        {x: 5, y: {high: 53, low: 45}},
+    ]];
+
+    expect(calculateAxisMinimum(singleRow, "x")).toBe(0);
+    expect(calculateAxisMinimum(multiRow, "x")).toBe(0);
+    expect(calculateAxisMinimum(bundledRow, "x")).toBe(5);
+    expect(calculateAxisMinimum(bundledRow, "y")).toBe(15);
+
+    expect(calculateAxisMaximum(singleRow, "x")).toBe(6);
+    expect(calculateAxisMaximum(multiRow, "x")).toBe(16);
+    expect(calculateAxisMaximum(bundledRow, "x")).toBe(5);
+    expect(calculateAxisMaximum(bundledRow, "y")).toBe(53);
+})
