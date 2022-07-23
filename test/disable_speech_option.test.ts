9392a30b07f159f923f896b5a786c7baf0fb4f13
Julianna Langston <julianna@cosaic.io>
2022-07-23 18:55:02 -0400

    Add test case for disabling speech

---
+++ test/disable_speech_option.test.ts
@@ -0,0 +1,37 @@
+import { c2mChart } from "../src/c2mChart";
+import { SUPPORTED_CHART_TYPES } from "../src/types";
+
+jest.useFakeTimers();
+window.AudioContext = jest.fn().mockImplementation(() => {
+    return {};
+});
+
+test("Move around by single events", () => {
+    const mockElement = document.createElement("div");
+    const mockElementCC = document.createElement("div");
+    const { err } = c2mChart({
+        type: SUPPORTED_CHART_TYPES.LINE,
+        data: [1, 2, 3, 0, 4, 5, 4, 3],
+        element: mockElement,
+        cc: mockElementCC,
+        options: {
+            enableSpeech: false,
+            enableSound: false
+        }
+    });
+    expect(err).toBe(null);
+
+    mockElement.dispatchEvent(new Event("focus"));
+
+    // Confirm that a summary was generated
+    expect(mockElementCC.textContent?.length).toBe(0);
+
+    // Move right
+    mockElement.dispatchEvent(
+        new KeyboardEvent("keydown", {
+            key: "ArrowRight"
+        })
+    );
+    jest.advanceTimersByTime(250);
+    expect(mockElementCC.textContent?.length).toBe(0);
+});
