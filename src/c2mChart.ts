d317465a0e677b106c08ecaf621366357e861656
Julianna Langston <julianna@cosaic.io>
2022-08-15 19:52:49 -0400

    Allow submiting on enter

--- src/c2mChart.ts
+++ src/c2mChart.ts
@@ -49,42 +49,56 @@ const launchOptionDialog = (
 
     <p tabIndex="0">While navigating this chart, you may find some sounds too low or too high to hear. Alternatively, you may want to expand the range of the sounds available. Use these sliders to adjust the range of sound:</p>
 
-    <div>
-        <label>
-            Lower hertz:
-            <input type="range" min="0" max="${
-                HERTZ.length - 1
-            }" step="1" id="lowerRange" value="${lower}" />
-        </label>
-    </div>
-    <div>
-        <label>
-            Upper hertz:
-            <input type="range" min="0" max="${
-                HERTZ.length - 1
-            }" step="1" id="upperRange" value="${upper}" />
-        </label>
-    </div>
+    <form id="optionForm">
+        <div>
+            <label>
+                Lower hertz:
+                <input type="range" min="0" max="${
+                    upper - 1
+                }" step="1" id="lowerRange" value="${lower}" />
+            </label>
+        </div>
+        <div>
+            <label>
+                Upper hertz:
+                <input type="range" min="${lower + 1}" max="${
+        HERTZ.length - 1
+    }" step="1" id="upperRange" value="${upper}" />
+            </label>
+        </div>
 
-    <button id="save">save</button>
+        <input id="save" type="submit" value="Save" />
+    </form>
     `;
 
     const lowerRange: HTMLInputElement = dialog.querySelector("#lowerRange");
     const upperRange: HTMLInputElement = dialog.querySelector("#upperRange");
 
-    dialog.querySelector("#save").addEventListener("click", () => {
+    const save = () => {
         const lowerValue = Number(lowerRange.value);
         const upperValue = Number(upperRange.value);
         cb(lowerValue, upperValue);
         esc();
+    };
+
+    dialog.querySelector("#optionForm").addEventListener("submit", (e) => {
+        e.preventDefault();
+        save();
+    });
+
+    dialog.querySelector("#save").addEventListener("click", (e) => {
+        e.preventDefault();
+        save();
     });
 
     if (playCb) {
         lowerRange.addEventListener("change", () => {
             playCb(Number(lowerRange.value));
+            upperRange.min = String(Number(lowerRange.value) + 1);
         });
         upperRange.addEventListener("change", () => {
             playCb(Number(upperRange.value));
+            lowerRange.max = String(Number(upperRange.value) - 1);
         });
     }
 
--- test/options_dialog.test.ts
+++ test/options_dialog.test.ts
@@ -211,4 +211,72 @@ test("In the Options dialog, the ranges play sounds onChange", () => {
     upperRange.dispatchEvent(new Event("change"));
     expect(playHistory.length).toBe(3);
     expect(lastFrequency).toBeCloseTo(1661.219);
+
+    document.querySelector("[role='dialog']")?.dispatchEvent(new Event("blur"));
+    expect(document.querySelectorAll("[role='dialog']").length).toBe(0);
+});
+
+test("In the Options dialog, modifying one range changes the limits of the other", () => {
+    const mockElement = document.createElement("div");
+    const mockElementCC = document.createElement("div");
+    const { err } = c2mChart({
+        type: SUPPORTED_CHART_TYPES.LINE,
+        data: [1, 2, 3, 0, 4, 5, 4, 3],
+        element: mockElement,
+        cc: mockElementCC,
+        audioEngine: new MockAudioEngine()
+    });
+    expect(err).toBe(null);
+
+    mockElement.dispatchEvent(new Event("focus"));
+
+    // Confirm that a summary was generated
+    expect(mockElementCC.textContent?.length).toBeGreaterThan(10);
+
+    mockElement.dispatchEvent(
+        new KeyboardEvent("keydown", {
+            key: " "
+        })
+    );
+    expect(playHistory.length).toBe(1);
+    expect(lastFrequency).toBeCloseTo(123.47);
+
+    expect(document.querySelectorAll("[role='dialog']").length).toBe(0);
+
+    mockElement.dispatchEvent(
+        new KeyboardEvent("keydown", {
+            key: "o"
+        })
+    );
+    expect(document.querySelectorAll("[role='dialog']").length).toBe(1);
+
+    const lowerRange = document.getElementById(
+        "lowerRange"
+    ) as HTMLInputElement;
+    expect(lowerRange).toHaveProperty("value", "21");
+    expect(lowerRange).toHaveProperty("min", "0");
+    expect(lowerRange).toHaveProperty("max", "95");
+    const upperRange = document.getElementById(
+        "upperRange"
+    ) as HTMLInputElement;
+    expect(upperRange).toHaveProperty("value", "96");
+    expect(upperRange).toHaveProperty("min", "22");
+    expect(upperRange).toHaveProperty("max", "107");
+
+    lowerRange.value = "30";
+    lowerRange.dispatchEvent(new Event("change"));
+    expect(upperRange).toHaveProperty("min", "31");
+
+    upperRange.value = "80";
+    upperRange.dispatchEvent(new Event("change"));
+    expect(lowerRange).toHaveProperty("max", "79");
+
+    document.querySelector("form")?.dispatchEvent(new Event("submit"));
+
+    mockElement.dispatchEvent(
+        new KeyboardEvent("keydown", {
+            key: " "
+        })
+    );
+    expect(lastFrequency).toBeCloseTo(155.56);
 });
