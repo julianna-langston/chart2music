e21760d06015b649a886f7d4c97c57f6120e5141
Julianna Langston <julianna@cosaic.io>
2022-09-14 15:49:35 -0400

    Allow users to provide custom hertz ranges

--- test/chart_audio.test.ts
+++ test/chart_audio.test.ts
@@ -7,6 +7,87 @@ window.AudioContext = jest.fn().mockImplementation(() => {
     return {};
 });
 
+const options = {
+    hertzes: [
+        55,
+        58.27047,
+        61.73541, // octave 1
+        65.40639,
+        69.29566,
+        73.41619,
+        77.78175,
+        82.40689,
+        87.30706,
+        92.49861,
+        97.99886,
+        103.8262,
+        110,
+        116.5409,
+        123.4708, // octave 2
+        130.8128,
+        138.5913,
+        146.8324,
+        155.5635,
+        164.8138,
+        174.6141,
+        184.9972,
+        195.9977,
+        207.6523,
+        220,
+        233.0819,
+        246.9417, // octave 3
+        261.6256,
+        277.1826,
+        293.6648,
+        311.127,
+        329.6276,
+        349.2282,
+        369.9944,
+        391.9954,
+        415.3047,
+        440,
+        466.1638,
+        493.8833, // octave 4
+        523.2511,
+        554.3653,
+        587.3295,
+        622.254,
+        659.2551,
+        698.4565,
+        739.9888,
+        783.9909,
+        830.6094,
+        880,
+        932.3275,
+        987.7666, // octave 5
+        1046.502,
+        1108.731,
+        1174.659,
+        1244.508,
+        1318.51,
+        1396.913,
+        1479.978,
+        1567.982,
+        1661.219,
+        1760,
+        1864.655,
+        1975.533, // octave 6
+        2093.005,
+        2217.461,
+        2349.318,
+        2489.016,
+        2637.02,
+        2793.826,
+        2959.955,
+        3135.963,
+        3322.438,
+        3520,
+        3729.31,
+        3951.066, // octave 7
+        4186.009
+    ]
+};
+
 beforeEach(() => {
     jest.clearAllMocks();
 });
@@ -70,7 +151,8 @@ test("Move around by single events - single line plot", () => {
         data: [1, 2, 3, 0, 4, 5, 4, 3],
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -128,7 +210,8 @@ test("Move around by single events - plot with stats", () => {
         },
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -169,7 +252,8 @@ test("Move around by single events - single line plot", () => {
         data: [1, 2, 3, 0, 4, 5, 4, 3],
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -227,7 +311,8 @@ test("Move around by single events - plot with y and y2", () => {
         },
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -269,7 +354,8 @@ test("Move around by single events - candlestick", () => {
         ],
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -295,7 +381,8 @@ test("Check play", () => {
         data: [1, 2, 3, 0, 4, 5, 4, 3],
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
@@ -389,7 +476,8 @@ test("Out of bounds data", () => {
                 maximum: 5
             }
         },
-        audioEngine: new MockAudioEngine()
+        audioEngine: new MockAudioEngine(),
+        options
     });
     expect(err).toBe(null);
 
