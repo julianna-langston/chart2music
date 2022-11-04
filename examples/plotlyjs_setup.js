86aac56745e5e4e3083cf1a0f2a53135b3c80260
Julianna Langston <julianna@cosaic.io>
2022-11-04 10:28:06 -0400

    Add Plotly boxplot example

--- examples/plotlyjs_setup.js
+++ examples/plotlyjs_setup.js
@@ -1,11 +1,12 @@
 import { example2 } from "./plotly/example2.js";
 import { example3 } from "./plotly/example3.js";
+import { example4 } from "./plotly/example4.js";
 
 window.addEventListener("load", () => {
     const grid = document.getElementById("examples");
     const cc = document.getElementById("cc");
 
-    const chartFn = [example2, example3];
+    const chartFn = [example2, example3, example4];
 
     chartFn.forEach((fn) => {
         const cell = document.createElement("div");
