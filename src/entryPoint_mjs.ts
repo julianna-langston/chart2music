ec519d3f9c19b562c0c1f5f01211dfae9fdbf5ba
Julianna Langston <julianna@cosaic.io>
2023-02-12 12:19:10 -0500

    Fix name for exported type

--- src/entryPoint_mjs.ts
+++ src/entryPoint_mjs.ts
@@ -1,6 +1,6 @@
 import { c2mChart, c2m } from "./c2mChart";
 
-export { SonifyTypes as C2MConfig } from "./types";
+export { SonifyTypes as C2MChartConfig } from "./types";
 
 export { c2m, c2mChart };
 export default c2mChart;
