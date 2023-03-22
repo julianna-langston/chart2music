8412c3ba3af7a511846007bbb7374c3700f67fc4
julianna-ciq <74684272+julianna-ciq@users.noreply.github.com>
2023-03-21 20:57:50 -0400

    Refactor audio tests (#179)

--- test/chart_audio_continuous.test.ts
+++ test/chart_audio_continuous.test.ts
@@ -1,146 +1,19 @@
-import type { AudioEngine } from "../src/audio";
 import { c2mChart } from "../src/c2mChart";
 import { SUPPORTED_CHART_TYPES } from "../src/types";
+import { MockAudioEngine } from "./_mockAudioEngine";
+import { hertzes } from "./_constants";
 
 jest.useFakeTimers();
 window.AudioContext = jest.fn().mockImplementation(() => {
     return {};
 });
 
-const options = {
-    hertzes: [
-        55,
-        58.27047,
-        61.73541, // octave 1
-        65.40639,
-        69.29566,
-        73.41619,
-        77.78175,
-        82.40689,
-        87.30706,
-        92.49861,
-        97.99886,
-        103.8262,
-        110,
-        116.5409,
-        123.4708, // octave 2
-        130.8128,
-        138.5913,
-        146.8324,
-        155.5635,
-        164.8138,
-        174.6141,
-        184.9972,
-        195.9977,
-        207.6523,
-        220,
-        233.0819,
-        246.9417, // octave 3
-        261.6256,
-        277.1826,
-        293.6648,
-        311.127,
-        329.6276,
-        349.2282,
-        369.9944,
-        391.9954,
-        415.3047,
-        440,
-        466.1638,
-        493.8833, // octave 4
-        523.2511,
-        554.3653,
-        587.3295,
-        622.254,
-        659.2551,
-        698.4565,
-        739.9888,
-        783.9909,
-        830.6094,
-        880,
-        932.3275,
-        987.7666, // octave 5
-        1046.502,
-        1108.731,
-        1174.659,
-        1244.508,
-        1318.51,
-        1396.913,
-        1479.978,
-        1567.982,
-        1661.219,
-        1760,
-        1864.655,
-        1975.533, // octave 6
-        2093.005,
-        2217.461,
-        2349.318,
-        2489.016,
-        2637.02,
-        2793.826,
-        2959.955,
-        3135.963,
-        3322.438,
-        3520,
-        3729.31,
-        3951.066, // octave 7
-        4186.009
-    ]
-};
+const audioEngine = new MockAudioEngine();
+const options = { hertzes };
 
 beforeEach(() => {
     jest.clearAllMocks();
-});
-
-/**
- * Info for play history
- */
-type playHistoryType = {
-    frequency: number;
-    panning: number;
-    duration: number;
-};
-
-let lastDuration = 0;
-let lastFrequency = 0;
-let lastPanning = 0;
-let playHistory: playHistoryType[] = [];
-
-/**
- * Mock audio engine. Built for testing purposes.
- */
-class MockAudioEngine implements AudioEngine {
-    masterGain: number;
-
-    /**
-     * Constructor
-     */
-    constructor() {
-        lastDuration = -10;
-        lastFrequency = -10;
-        lastPanning = -10;
-    }
-
-    /**
-     * The instructions to play a data point. The details are being recorded for the test system.
-     *
-     * @param frequency - hertz to play
-     * @param panning - panning (-1 to 1) to play at
-     * @param duration - how long to play
-     */
-    playDataPoint(frequency: number, panning: number, duration: number): void {
-        lastFrequency = frequency;
-        lastPanning = panning;
-        lastDuration = duration;
-        playHistory.push({ frequency, panning, duration });
-    }
-}
-
-beforeEach(() => {
-    playHistory = [];
-    lastDuration = -10;
-    lastFrequency = -10;
-    lastPanning = -10;
+    audioEngine.reset();
 });
 
 test("Continuous-mode: Move around by single events - single line plot", () => {
@@ -183,7 +56,7 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         },
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine(),
+        audioEngine,
         options
     });
     expect(err).toBe(null);
@@ -199,8 +72,8 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         })
     );
     jest.advanceTimersByTime(250);
-    expect(lastPanning).toBe(-0.98);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.lastPanning).toBe(-0.98);
+    expect(audioEngine.playCount).toBe(1);
 
     mockElement.dispatchEvent(
         new KeyboardEvent("keydown", {
@@ -209,22 +82,22 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         })
     );
     jest.advanceTimersByTime(100);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(800);
-    expect(playHistory).toHaveLength(3);
+    expect(audioEngine.playCount).toBe(3);
 
     jest.advanceTimersByTime(500);
-    expect(playHistory).toHaveLength(6);
+    expect(audioEngine.playCount).toBe(6);
 
     // Middle of the pause between x=5 and x=10. Shouldn't play anything
     jest.advanceTimersByTime(500);
-    expect(playHistory).toHaveLength(6);
+    expect(audioEngine.playCount).toBe(6);
 
     jest.advanceTimersByTime(2000);
-    expect(playHistory).toHaveLength(7);
+    expect(audioEngine.playCount).toBe(7);
 
-    playHistory = [];
+    audioEngine.reset();
     mockElement.dispatchEvent(
         new KeyboardEvent("keydown", {
             key: "Home",
@@ -232,15 +105,15 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         })
     );
     jest.advanceTimersByTime(100);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 
     jest.advanceTimersByTime(500);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 
     jest.advanceTimersByTime(2000);
-    expect(playHistory).toHaveLength(6);
+    expect(audioEngine.playCount).toBe(6);
 
-    playHistory = [];
+    audioEngine.reset();
     mockElement.dispatchEvent(
         new KeyboardEvent("keydown", {
             key: "End",
@@ -248,7 +121,7 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         })
     );
     jest.advanceTimersByTime(100);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 
     mockElement.dispatchEvent(
         new KeyboardEvent("keydown", {
@@ -257,7 +130,7 @@ test("Continuous-mode: Move around by single events - single line plot", () => {
         })
     );
     jest.advanceTimersByTime(5000);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 });
 
 // grouped line plot
@@ -298,7 +171,7 @@ test("C2M sorts out-of-order scatter plot data", () => {
         },
         element: mockElement,
         cc: mockElementCC,
-        audioEngine: new MockAudioEngine(),
+        audioEngine,
         options
     });
     expect(err).toBe(null);
@@ -317,24 +190,24 @@ test("C2M sorts out-of-order scatter plot data", () => {
         })
     );
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 
     jest.advanceTimersByTime(800);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(3);
+    expect(audioEngine.playCount).toBe(3);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(3);
+    expect(audioEngine.playCount).toBe(3);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(4);
+    expect(audioEngine.playCount).toBe(4);
 
-    playHistory = [];
+    audioEngine.reset();
 
     mockElement.dispatchEvent(
         new KeyboardEvent("keydown", {
@@ -343,26 +216,26 @@ test("C2M sorts out-of-order scatter plot data", () => {
         })
     );
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(1);
+    expect(audioEngine.playCount).toBe(1);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(2);
+    expect(audioEngine.playCount).toBe(2);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(3);
+    expect(audioEngine.playCount).toBe(3);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(3);
+    expect(audioEngine.playCount).toBe(3);
 
     jest.advanceTimersByTime(250);
-    expect(playHistory).toHaveLength(4);
+    expect(audioEngine.playCount).toBe(4);
 });
