3758da4a84bdce56e744f214875ab87fc29f34af
Julianna Langston <julie.langston@gmail.com>
2023-10-03 20:55:52 -0400

    Cleanup tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -850,5 +850,7 @@ describe("utils", () => {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
         );
         expect(detectIfMobile()).toBeFalsy();
+
+        jest.clearAllMocks();
     });
 });
