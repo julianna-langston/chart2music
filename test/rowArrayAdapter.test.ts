1134d0b69c509187274952bbefe739b10d1d065c
Andrew Pikul <ajpikul@gmail.com>
2024-03-17 19:44:17 -0500

    Fix arrayAdapter tests to test more data

--- test/rowArrayAdapter.test.ts
+++ test/rowArrayAdapter.test.ts
@@ -2,9 +2,11 @@ import * as adapter from "../src/rowArrayAdapter";
 import type { SimpleDataPoint } from "../src/dataPoint";
 import { convertDataRow } from "../src/utils";
 
-const plainArray = [1123]; // , 2342, 301823, 409];
-const adaptedArray = new adapter.ArrayAsAdapter<number>(plainArray)
-const simplePointArray = new adapter.ArrayAsAdapter<SimpleDataPoint>(convertDataRow(plainArray))
+const plainArray = [1123, 2342, 301823, 409];
+const adaptedArray = new adapter.ArrayAsAdapter<number>(plainArray);
+const simplePointArray = new adapter.ArrayAsAdapter<SimpleDataPoint>(
+    convertDataRow(plainArray)
+);
 
 test("test if ArrayAsAdapter successful constructs a proper RowArrayAdapter", () => {
     expect(adapter.isRowArrayAdapter(plainArray)).toBeFalsy();
@@ -20,7 +22,7 @@ test("test if ArrayAsAdapter successful constructs a proper RowArrayAdapter", ()
     expect(adaptedArray.min("")).toBe(Math.min(...plainArray));
     expect(simplePointArray.min("x")).toBe(0);
     expect(simplePointArray.min("y")).toBe(Math.min(...plainArray));
-    expect(simplePointArray.max("x")).toBe(plainArray.length-1);
+    expect(simplePointArray.max("x")).toBe(plainArray.length - 1);
     expect(simplePointArray.max("y")).toBe(Math.max(...plainArray));
 
     // Test at(i)
@@ -50,31 +52,31 @@ test("test if ArrayAsAdapter successful constructs a proper RowArrayAdapter", ()
     });
 
     // Test forEach:
-    adaptedArray.forEach(function(element, index, array) {
-      expect(element).toBe(plainArray[index]);
-      expect(array).toBe(adaptedArray);
-      expect(this).toBe("this replaces `this`");
-    }, "this replaces `this`")
+    adaptedArray.forEach(function (element, index, array) {
+        expect(element).toBe(plainArray[index]);
+        expect(array).toBe(adaptedArray);
+        expect(this).toBe("this replaces `this`");
+    }, "this replaces `this`");
     // NOTE: can't use => if we want to replace this
     // because => does a permanent bind
-    adaptedArray.forEach(function(element, index, array) {
-      expect(element).toBe(plainArray[index]);
-      expect(array).toBe(adaptedArray);
-      // an unspecified value of `this` will be `undefined`
-      // in a testing context, but in a browser it will depend on
-      // whether 'use strict`; Testing for it may be prudent.
-    })
-    simplePointArray.forEach(function(element, index, array) {
-      expect(element.y).toBe(plainArray[index]);
-      expect(array).toBe(simplePointArray);
-      expect(this).toBe("this replaces `this`");
-    }, "this replaces `this`")
-    simplePointArray.forEach(function(element, index, array) {
-      expect(element.y).toBe(plainArray[index]);
-      expect(array).toBe(simplePointArray);
-    })
+    adaptedArray.forEach(function (element, index, array) {
+        expect(element).toBe(plainArray[index]);
+        expect(array).toBe(adaptedArray);
+        // an unspecified value of `this` will be `undefined`
+        // in a testing context, but in a browser it will depend on
+        // whether 'use strict`; Testing for it may be prudent.
+    });
+    simplePointArray.forEach(function (element, index, array) {
+        expect(element.y).toBe(plainArray[index]);
+        expect(array).toBe(simplePointArray);
+        expect(this).toBe("this replaces `this`");
+    }, "this replaces `this`");
+    simplePointArray.forEach(function (element, index, array) {
+        expect(element.y).toBe(plainArray[index]);
+        expect(array).toBe(simplePointArray);
+    });
 
-   // Implement:
+    // Implement:
     // [x] length
     // [x] indexOf
     // [x] min
