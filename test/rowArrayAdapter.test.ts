fd0334dabab57ae710cde368bb714d5a5ca0921d
Andrew Pikul <ajpikul@gmail.com>
2024-03-17 19:42:19 -0500

    Test new min/max and forEach functions, reorganize

--- test/rowArrayAdapter.test.ts
+++ test/rowArrayAdapter.test.ts
@@ -2,30 +2,87 @@ import * as adapter from "../src/rowArrayAdapter";
 import type { SimpleDataPoint } from "../src/dataPoint";
 import { convertDataRow } from "../src/utils";
 
+const plainArray = [1123]; // , 2342, 301823, 409];
+const adaptedArray = new adapter.ArrayAsAdapter<number>(plainArray)
+const simplePointArray = new adapter.ArrayAsAdapter<SimpleDataPoint>(convertDataRow(plainArray))
+
 test("test if ArrayAsAdapter successful constructs a proper RowArrayAdapter", () => {
-    const x = [1, 2, 3, 4];
-    expect(adapter.isRowArrayAdapter(x)).toBeFalsy();
-    const xAdapter = new adapter.ArrayAsAdapter<SimpleDataPoint>(
-        convertDataRow(x)
-    );
-    expect(adapter.isRowArrayAdapter(xAdapter)).toBeTruthy();
-    expect(xAdapter.length).toBe(x.length);
-    // expect(xAdapter.min()).toBe(Math.min(...x)); // not yet
-    // expect(xAdapter.max()).toBe(Math.max(...x)); // not yet
-    x.forEach((el, i) => {
-        expect(xAdapter.at(i).y).toBe(el);
-        expect(xAdapter.at(i).x).toBe(i);
+    expect(adapter.isRowArrayAdapter(plainArray)).toBeFalsy();
+    expect(adapter.isRowArrayAdapter(adaptedArray)).toBeTruthy();
+
+    // Test length
+    expect(adaptedArray.length).toBe(plainArray.length);
+    expect(simplePointArray.length).toBe(plainArray.length);
+
+    // Test min/max
+    expect(adaptedArray.min("x")).toBe(NaN);
+    expect(adaptedArray.max("y")).toBe(NaN);
+    expect(adaptedArray.min("")).toBe(Math.min(...plainArray));
+    expect(simplePointArray.min("x")).toBe(0);
+    expect(simplePointArray.min("y")).toBe(Math.min(...plainArray));
+    expect(simplePointArray.max("x")).toBe(plainArray.length-1);
+    expect(simplePointArray.max("y")).toBe(Math.max(...plainArray));
+
+    // Test at(i)
+    plainArray.forEach((el, i) => {
+        expect(adaptedArray.at(i)).toBe(el);
+        expect(simplePointArray.at(i).x).toBe(i);
+        expect(simplePointArray.at(i).y).toBe(el);
     });
-    x.forEach((el, i) => {
+
+    // Test indexOf(i)
+    plainArray.forEach((val, i) => {
         expect(
-            xAdapter.findIndex((el2: SimpleDataPoint) => {
-                return el2.y === el;
+            adaptedArray.findIndex((point: number) => {
+                return point === val;
             })
         ).toBe(i);
         expect(
-            xAdapter.findIndex(() => {
+            simplePointArray.findIndex((point: SimpleDataPoint) => {
+                return point.y === val;
+            })
+        ).toBe(i);
+        expect(
+            simplePointArray.findIndex(() => {
                 return false;
             })
         ).toBe(-1);
     });
+
+    // Test forEach:
+    adaptedArray.forEach(function(element, index, array) {
+      expect(element).toBe(plainArray[index]);
+      expect(array).toBe(adaptedArray);
+      expect(this).toBe("this replaces `this`");
+    }, "this replaces `this`")
+    // NOTE: can't use => if we want to replace this
+    // because => does a permanent bind
+    adaptedArray.forEach(function(element, index, array) {
+      expect(element).toBe(plainArray[index]);
+      expect(array).toBe(adaptedArray);
+      // an unspecified value of `this` will be `undefined`
+      // in a testing context, but in a browser it will depend on
+      // whether 'use strict`; Testing for it may be prudent.
+    })
+    simplePointArray.forEach(function(element, index, array) {
+      expect(element.y).toBe(plainArray[index]);
+      expect(array).toBe(simplePointArray);
+      expect(this).toBe("this replaces `this`");
+    }, "this replaces `this`")
+    simplePointArray.forEach(function(element, index, array) {
+      expect(element.y).toBe(plainArray[index]);
+      expect(array).toBe(simplePointArray);
+    })
+
+   // Implement:
+    // [x] length
+    // [x] indexOf
+    // [x] min
+    // [x] max
+    // [x] at
+    // [x] forEach
+    // [ ] map
+    // [ ] filter
+    // [ ] for(in of)
+    // [ ] reduce
 });
