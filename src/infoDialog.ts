b838ad87b094ec81d756c01982a3632262db5300
Rob Bruckheimer <rob@chartiq.com>
2025-04-24 11:29:30 -0400

    Fix tabbing/spacing

--- src/infoDialog.ts
+++ src/infoDialog.ts
@@ -11,21 +11,21 @@ export const launchInfoDialog = (
     const dialog = document.createElement("dialog");
     dialog.classList.add("chart2music-dialog");
     dialog.classList.add("chart2music-info-dialog");
-	const translatedInfoTitle = translationCallback("info-title");
+    const translatedInfoTitle = translationCallback("info-title");
     dialog.setAttribute("aria-label", translatedInfoTitle);
-	const h1 = dialog.appendChild(document.createElement("h1"));
-	h1.tabIndex = 0;
-	h1.textContent = translatedInfoTitle;
+    const h1 = dialog.appendChild(document.createElement("h1"));
+    h1.tabIndex = 0;
+    h1.textContent = translatedInfoTitle;
 
     if ("notes" in info) {
-		const h2 = dialog.appendChild(document.createElement("h2"));
-		h2.textContent = translationCallback("info-notes");
+        const h2 = dialog.appendChild(document.createElement("h2"));
+        h2.textContent = translationCallback("info-notes");
 
-		const ul = dialog.appendChild(document.createElement("ul"));
+        const ul = dialog.appendChild(document.createElement("ul"));
         info.notes.forEach((str) => {
-			const li = ul.appendChild(document.createElement("li"));
-			li.textContent = str;
-		});
+            const li = ul.appendChild(document.createElement("li"));
+	         li.textContent = str;
+        });
     }
 
     document.body.appendChild(dialog);
